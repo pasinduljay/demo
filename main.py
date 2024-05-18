2fd3276242d8869f09d2ed4b76d77792878f9b3b
pasinduljay <pasindulakshitha0822@gmail.com>
2024-05-18 17:22:57 +0530

    Add pytest to requirements and a sample test

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
-app.config['SECRET_KEY'] = 'your_secret_key'
 
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-
-class UserForm(FlaskForm):
-    username = StringField('Username')
-    submit = SubmitField('Submit')
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    form = UserForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        new_user = User(username=username)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('home'))
-    users = User.query.all()
-    return render_template('index.html', form=form, users=users)
+    return 'Hello, world! This is my Flask web app.'
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
--- test_main.py
+++ test_main.py
@@ -1,2 +1,9 @@
-def test_example():
-    assert 1 + 1 == 2
+from main import app
+
+def test_home():
+    app.testing = True
+    client = app.test_client()
+    
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello, world! This is my Flask web app.'
